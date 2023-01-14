1834afdbaf232891eb334bca69855e58699766e3
Iza Majchrowska <95052426+xizula@users.noreply.github.com>
2023-01-14 15:19:59 +0100

    Delete php directory

--- php/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","admin","mtg");
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " ;
-}
-?>
--- php/getlogindata.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(isset($_REQUEST['login'])) {
-    $username = $_REQUEST['username'];
-    $password = $_REQUEST['password'];
-}
-?>
